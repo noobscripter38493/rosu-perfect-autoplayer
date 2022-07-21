2d1de90a012590d0c05374b184f2007fd6e66c72
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2022-07-20 23:52:55 -0400

    Update rosu auto player.lua

--- rosu auto player.lua
+++ rosu auto player.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("currently patched, working on a fix")
+game.Players.LocalPlayer:Kick("currently patched, hold notes are the only thing currently broken: https://streamable.com/g39x5w")
